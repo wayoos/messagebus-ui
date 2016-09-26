030d8571646065c7a3170a407f76cddb880aa0e5
Stephane Cusin <stephane.cusin@wayoos.com>
2016-09-27 00:26:19 +0200

    cleanup project, recreate from angular.io tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,8 @@
 import { Component } from '@angular/core';
-
+import '../../public/css/styles.css';
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
-
-  views: Object[] = [
-    {
-      name: 'My account',
-      description: 'Edit my account information',
-      icon: 'assignment ind'
-    },
-    {
-      name: 'Potential dates',
-      description: 'Find your ',
-      icon: 'pets'
-    }
-  ];
-
-  dogs: Object[] = [
-    {name: 'Porter'},
-    {name: 'Mal'},
-    {name: 'Razzle'},
-    {name: 'Koby'},
-    {name: 'Molly'},
-    {name: 'Husi'}
-  ];
-
-}
+export class AppComponent { }
